c1c6218320d85d98019eb462b0dc200182ff2b51
Kenzhu02 <99104107+Kenzhu02@users.noreply.github.com>
2022-03-15 20:25:36 +0800

    Update maintenance.py

--- Music/Plugins/maintenance.py
+++ Music/Plugins/maintenance.py
@@ -5,9 +5,9 @@ from Music.MusicUtilities.database.onoff import (is_on_off, add_on, add_off)
 from Music.MusicUtilities.helpers.filters import command
 
 
-@Client.on_message(command("Musicp") & filters.user(SUDOERS))
+@Client.on_message(command("Musik") & filters.user(SUDOERS))
 async def smex(_, message):
-    usage = "**Usage:**\n/Musicp [enable|disable]"
+    usage = "**Gunakan :**\n/Musik [enable|disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     chat_id = message.chat.id
@@ -16,16 +16,16 @@ async def smex(_, message):
     if state == "enable":
         user_id = 1
         await add_on(user_id)
-        await message.reply_text("Music Enabled for Maintenance")
+        await message.reply_text("Mode perbaikan Musik **AKTIF** ")
     elif state == "disable":
         user_id = 1
         await add_off(user_id)
-        await message.reply_text("Maintenance Mode Disabled")
+        await message.reply_text("Mode perbaikan Musik **TIDAK AKTIF**")
     else:
         await message.reply_text(usage)
 
         
-@Client.on_message(command("stp") & filters.user(SUDOERS))
+@Client.on_message(command("str") & filters.user(SUDOERS))
 async def sls_skfs(_, message):
     usage = "**Usage:**\n/st [enable|disable]"
     if len(message.command) != 2:
